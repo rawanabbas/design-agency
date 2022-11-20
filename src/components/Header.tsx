8592ac440f9b06e10a13df335770047221b52d6c
Rawan Abbas <rawanabbas@aucegypt.edu>
2022-11-20 17:34:19 -0500

    Responsive Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,14 +3,26 @@ import styles from '../modules/Header.module.css'
 import logo from '../assets/logo.svg';
 import { NavHashLink } from 'react-router-hash-link';
 
-export default class Header extends Component {
-  scrolled: boolean = false;
-  header: any;
-  sticky: Number = 0;
+interface IHeaderProps {
 
-  Header() {
+}
+interface IHeaderState {
+  open?: boolean;
+}
+export default class Header extends Component<IHeaderProps, IHeaderState> {
+  scrolled: boolean = false;
+  header: HTMLElement | any;
+  sticky: Number = 0;
+  menuBtn: HTMLElement | any;
+
+  constructor(props : IHeaderProps) {
+    super(props);
     this.header = document.getElementById("header");
+    this.menuBtn = document.getElementById("menu-btn");
     this.sticky = this.header?.offsetTop;
+    this.state = {
+      open: false
+    }
   }
 
   componentDidMount(): void {
@@ -33,12 +45,19 @@ export default class Header extends Component {
     
   }
 
+  toggleMenu = () => {
+    this.setState({open: !this.state.open});
+    window.requestAnimationFrame(() => {
+      document?.getElementById("menu-btn")?.classList.toggle(styles.menu_btn_open);
+    })
+  }
+
   render() {
     return (
       <div className={`${styles.header}`} id="header">
-        <div className={styles.content}>
+        <div className={`${styles.content} ${this.state.open ? styles.full_header : ''}`}>
           <img src={logo} alt="Logo" className={styles.logo}/>
-          <div className={styles.menu}>
+          <div className={`${styles.menu} ${!this.state.open ? styles.menu_resp : ''}`}>
               <ul>
                 <li><NavHashLink scroll={(el) => el.scrollIntoView({ behavior: 'smooth' })} to="#home">Home</NavHashLink></li>
                 <li><NavHashLink scroll={(el) => el.scrollIntoView({ behavior: 'smooth' })} to="#info">Your Benefits</NavHashLink></li>
@@ -47,8 +66,11 @@ export default class Header extends Component {
                 <li><NavHashLink scroll={(el) => el.scrollIntoView({ behavior: 'smooth' })} to="#work">Our Work</NavHashLink></li>
               </ul>
           </div>
-          <button className={styles['action-btn']}>Get Started</button>
+          <button className={`${styles['action-btn']} ${!this.state.open ? styles['action-btn_resp'] : ''}`}>Get Started</button>
         </div>
+          <div className={styles.menu_btn} onClick={this.toggleMenu} id="menu-btn">
+            <div className={styles.menu_line}></div>
+          </div>
       </div>
     )
   }
